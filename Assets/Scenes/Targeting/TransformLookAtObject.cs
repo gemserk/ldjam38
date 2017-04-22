63931e1fcaf80110a12321fb5d8f143d8aeb14ca
Ariel Coppes <ariel.coppes@gmail.com>
2017-04-22 18:31:29 -0300

    configured camera position and angle, etc

---
+++ Assets/Scenes/Targeting/TransformLookAtObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class TransformLookAtObject : MonoBehaviour {
+
+	public Transform controlObject;
+	public Transform followObject;
+
+	// Update is called once per frame
+	void Update () {
+		if (controlObject == null)
+			return;
+		if (followObject == null)
+			return;
+		controlObject.transform.LookAt (followObject.position, followObject.up);
+	}
+}
