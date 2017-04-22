b9b355cb945001113b78c4f5db90a71fffb923d5
Ruben Garat <ruben01@gmail.com>
2017-04-22 18:34:15 -0300

    added weapon

---
+++ Assets/Scripts/Game/Weapons/Bomb.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game.Weapons
+{
+    public class Bomb : MonoBehaviour
+    {
+        public float explodeRadius;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, explodeRadius);
+            foreach (var collider in hitColliders)
+            {
+                if (collider.gameObject == this.gameObject) continue;
+
+                GameObject.Destroy(collider.gameObject);
+            }
+        }
+    }
+}
