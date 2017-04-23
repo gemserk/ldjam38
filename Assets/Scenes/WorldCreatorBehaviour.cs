6b7a96c6bb8c74ca7d52219309907b0af3ebb20c
Ruben Garat <ruben01@gmail.com>
2017-04-22 22:39:37 -0300

    added materials, added truch props, trying to create level

--- Assets/Scenes/WorldCreatorBehaviour.cs
+++ Assets/Scenes/WorldCreatorBehaviour.cs
@@ -1,43 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using Gemserk.Utils;
+using UnityEngine;
 
-public class WorldCreator
+namespace Gemserk
 {
-	public void CreateWorld(Transform parent, GameObject cubePrefab, Vector3 worldSize)
-	{
-		var collider = cubePrefab.GetComponent<BoxCollider> ();
-		var cubeSize = collider.size;
+    public class WorldCreator
+    {
+        public void CreateWorld(Transform parent, GameObject cubePrefab, Vector3 worldSize)
+        {
+            var collider = cubePrefab.GetComponent<BoxCollider>();
+            var cubeSize = collider.size;
 
-		float x = (-worldSize.x * cubeSize.x) * 0.5f;
-		float y = (-worldSize.y * cubeSize.y) * 0.5f;
-		float z = (-worldSize.z * cubeSize.z) * 0.5f;
+            float x = (-worldSize.x * cubeSize.x) * 0.5f;
+            float y = (-worldSize.y * cubeSize.y) * 0.5f;
+            float z = (-worldSize.z * cubeSize.z) * 0.5f;
 
-		for (int i = 0; i < worldSize.x; i++) {
-			y = (-worldSize.y * cubeSize.y) * 0.5f;
-			for (int j = 0; j < worldSize.y; j++) {
-				z = (-worldSize.z * cubeSize.z) * 0.5f;
-				for (int k = 0; k < worldSize.z; k++) {
-					var cubeObject = GameObject.Instantiate (cubePrefab, parent);
-					cubeObject.transform.localPosition = new Vector3 (x, y, z);
-					z += cubeSize.z;
-				}
-				y += cubeSize.y;
-			}
-			x += cubeSize.x;
-		}
-	}
-}
+            for (int i = 0; i < worldSize.x; i++)
+            {
+                y = (-worldSize.y * cubeSize.y) * 0.5f;
+                for (int j = 0; j < worldSize.y; j++)
+                {
+                    z = (-worldSize.z * cubeSize.z) * 0.5f;
+                    for (int k = 0; k < worldSize.z; k++)
+                    {
+                        var cubeObject = GameObject.Instantiate(cubePrefab, parent);
+                        cubeObject.transform.localPosition = new Vector3(x, y, z);
+                        cubeObject.gameObject.name = String.Format("{0}-{1}-{2} - ({3},{4},{5})", (int)i, j, k, (int)worldSize.x,
+                            (int)worldSize.y, (int)worldSize.z);
+                        z += cubeSize.z;
+                    }
+                    y += cubeSize.y;
+                }
+                x += cubeSize.x;
+            }
+        }
+    }
 
-public class WorldCreatorBehaviour : MonoBehaviour {
+    public class WorldCreatorBehaviour : MonoBehaviour
+    {
 
-	public GameObject cubePrefab;
+        public GameObject cubePrefab;
 
-	[Tooltip("No sea nabo, es vector de enteros")]
-	public Vector3 worldSize;
+        [Tooltip("No sea nabo, es vector de enteros")] public Vector3 worldSize;
 
-	// Use this for initialization
-	void Start () {
-		var worldCreator = new WorldCreator ();
-		worldCreator.CreateWorld (this.transform, cubePrefab, worldSize);	
-	}
+        // Use this for initialization
+        void Start()
+        {
+            Regenerate();
+        }
+
+        [ContextMenu("Regenerate")]
+        void Regenerate()
+        {
+            var worldCreator = new WorldCreator();
+
+            this.transform.GetChildren(false).ToList().ForEach(transform1 => GameObject.DestroyImmediate(transform1.gameObject));
+
+            worldCreator.CreateWorld(this.transform, cubePrefab, worldSize);
+        }
+
+    }
 
 }
---
+++ Assets/Scripts/Utils/TransformExtensions.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace Gemserk.Utils
+{
+
+
+    public static class TransformExtension
+    {
+        /// <summary>
+        /// Gives an enumerator for the direct children of a transform, generates garbage
+        /// </summary>
+        /// <param name="includeCurrent">if true includes the current transform in the enumeration</param>
+        public static IEnumerable<Transform> GetChildren(this Transform theTransform, bool includeCurrent)
+        {
+            if (includeCurrent)
+            {
+                yield return theTransform;
+            }
+
+            for (int i = 0; i < theTransform.childCount; i++)
+            {
+                var child = theTransform.GetChild((i));
+                yield return child;
+            }
+        }
+
+        /// <summary>
+        /// Gives an enumerator for all the decendants of a transform, generates garbage
+        /// </summary>
+        /// <param name="includeCurrent">if true includes the current transform in the enumeration</param>
+        public static IEnumerable<Transform> GetChildrenRecursive(this Transform theTransform, bool includeCurrent)
+        {
+            if (includeCurrent)
+            {
+                yield return theTransform;
+            }
+
+            for (int i = 0; i < theTransform.childCount; i++)
+            {
+                var child = theTransform.GetChild((i));
+                foreach (var grandson in GetChildrenRecursive(child, true))
+                {
+                    yield return grandson;
+                }
+            }
+        }
+
+        public static void ResetLocal(this Transform t)
+        {
+            t.localPosition = Vector3.zero;
+            t.localRotation = Quaternion.identity;
+            t.localScale = Vector3.one;
+        }
+
+
+
+
+        #region Collections
+
+        public static void MergeWith<K, V>(this Dictionary<K, V> orig, Dictionary<K, V> newData, bool replaceDuplicates)
+        {
+            if (replaceDuplicates)
+            {
+                foreach (var entry in newData)
+                {
+                    orig[entry.Key] = entry.Value;
+                }
+            }
+            else
+            {
+                foreach (var entry in newData)
+                {
+                    orig.Add(entry.Key, entry.Value);
+                }
+            }
+        }
+
+
+        public static void RemoveAsBag<T>(this List<T> list, T item)
+        {
+            int indexOf = list.IndexOf(item);
+            if (indexOf == -1)
+                return;
+
+            var lastIndex = list.Count - 1;
+
+            list[indexOf] = list[lastIndex];
+            list.RemoveAt(lastIndex);
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,TKey key,TValue defaultValue)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public static TValue GetValueOrDefaultFunc<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,TKey key,Func<TValue> defaultValueProvider)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value
+                : defaultValueProvider();
+        }
+
+        public static TValue GetOrNew<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,TKey key) where TValue:new()
+        {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+            {
+                value = new TValue();
+                dictionary.Add(key, value);
+            }
+
+            return value;
+        }
+
+        public static string ToStringArray<T>(this IEnumerable<T> enumerable, string separator, Func<T, string> func){
+            return String.Join (separator, enumerable.Select (func).ToArray ());
+        }
+
+        public static string ToStringArray<T>(this IEnumerable<T> enumerable, string separator){
+            return String.Join (separator, enumerable.Select ((arg) => arg.ToString()).ToArray ());
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> ie, Action<T> action)
+        {
+            foreach (var i in ie)
+            {
+                action(i);
+            }
+        }
+
+        #endregion
+
+        #region MyRegion
+
+        public static string ToStringDetailed(this Vector2 v) {
+            return String.Format ("{{{0}, {1}}}", v.x, v.y);
+        }
+
+        public static Vector2 ScalarMultiplication(this Vector2 v, Vector2 other)
+        {
+            return new Vector2(v.x * other.x, v.y * other.y);
+        }
+
+        #endregion
+
+
+    }
+}
