8eb48a8cf672fae61834a8f07bae5bafdaa904a1
Ariel Coppes <ariel.coppes@gmail.com>
2017-04-23 17:54:34 -0300

    extracted charge indicator base class

--- Assets/Scenes/AttackScene/ChargeIndicator.cs
+++ Assets/Scenes/AttackScene/ChargeIndicator.cs
@@ -1,32 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class ChargeIndicator : MonoBehaviour {
-
-	public Transform indicatorTransform;
-
-	public float minScale = 0.0f;
-	public float maxScale = 2.0f;
-
-	public Gradient gradient;
-
-	public Renderer indicatorRenderer;
-
-	public void UpdateCharge (WeaponControl weaponControl)
-	{
-		float charge = weaponControl.GetCharge ();
-		var weapon = weaponControl.GetWeapon ();
-
-		var localScale = indicatorTransform.localScale;
-		localScale.z = Mathf.Lerp (minScale, maxScale, charge);
-		indicatorTransform.localScale = localScale;
-
-		indicatorTransform.position = weaponControl.GetWeapon ().GetFireTransform ().position;
-		indicatorTransform.forward = weaponControl.GetWeapon ().GetFireTransform ().forward;
-
-		if (indicatorRenderer != null) {
-			indicatorRenderer.material.color = gradient.Evaluate (charge);
-		}
-
-	}
-
+public abstract class ChargeIndicator : MonoBehaviour
+{
+	public abstract void UpdateCharge (WeaponControl weaponControl);
 }
---
+++ Assets/Scenes/AttackScene/ChargeIndicatorSimple.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ChargeIndicatorSimple : ChargeIndicator {
+
+	public Transform indicatorTransform;
+
+	public float minScale = 0.0f;
+	public float maxScale = 2.0f;
+
+	public Gradient gradient;
+
+	public Renderer indicatorRenderer;
+
+	public override void UpdateCharge (WeaponControl weaponControl)
+	{
+		float charge = weaponControl.GetCharge ();
+		var weapon = weaponControl.GetWeapon ();
+
+		var localScale = indicatorTransform.localScale;
+		localScale.z = Mathf.Lerp (minScale, maxScale, charge);
+		indicatorTransform.localScale = localScale;
+
+		indicatorTransform.position = weaponControl.GetWeapon ().GetFireTransform ().position;
+		indicatorTransform.forward = weaponControl.GetWeapon ().GetFireTransform ().forward;
+
+		if (indicatorRenderer != null) {
+			indicatorRenderer.material.color = gradient.Evaluate (charge);
+		}
+
+	}
+
+}
