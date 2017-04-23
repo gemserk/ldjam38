93a947262d1fc45b82b256a3e578a6fa65f5dc33
Ariel Coppes <ariel.coppes@gmail.com>
2017-04-23 10:39:32 -0300

    changed to use character direction for movement

--- Assets/Scenes/SimpleMovement/SimpleMovementCharacter.cs
+++ Assets/Scenes/SimpleMovement/SimpleMovementCharacter.cs
@@ -4,34 +4,28 @@ public class SimpleMovementCharacter : MonoBehaviour
 {
 	public GameObject character;
 
-	// TODO: use character looking direction too to consider left/right
-
 	public WorldMovement worldMovement;
 
 	public int heightTolerance = 1;
 
 	public void MoveLeft ()
 	{
-		character.transform.position = worldMovement.Move(character.transform.position, new Vector3(1, 0, 0), heightTolerance);
-//		character.transform.position += new Vector3 (movementVector.x, 0, 0);
+		character.transform.position = worldMovement.Move(character.transform.position, character.transform.right, heightTolerance);
 	}
 
 	public void MoveRight()
 	{
-		character.transform.position = worldMovement.Move(character.transform.position, new Vector3(-1, 0, 0), heightTolerance);
-//		character.transform.position += new Vector3 (-movementVector.x, 0, 0);
+		character.transform.position = worldMovement.Move(character.transform.position, character.transform.right * -1, heightTolerance);
 	}
 
 	public void MoveForward()
 	{
-		character.transform.position = worldMovement.Move(character.transform.position, new Vector3(0, 0, 1), heightTolerance);
-//		character.transform.position += new Vector3 (0, 0, movementVector.z);
+		character.transform.position = worldMovement.Move(character.transform.position, character.transform.forward, heightTolerance);
 	}
 
 	public void MoveBackwards()
 	{
-		character.transform.position = worldMovement.Move(character.transform.position, new Vector3(0, 0, -1), heightTolerance);
-//		character.transform.position += new Vector3 (0, 0, -movementVector.z);
+		character.transform.position = worldMovement.Move(character.transform.position, character.transform.forward * -1, heightTolerance);
 	}
 	
 }
