a776609cf326927cee8dbedadd8397c3aac39b13
Ariel Coppes <ariel.coppes@gmail.com>
2017-04-23 01:19:37 -0300

    added movement between blocks with different height tolerance

--- Assets/Scenes/SimpleMovement/WorldMovementBlocks.cs
+++ Assets/Scenes/SimpleMovement/WorldMovementBlocks.cs
@@ -11,11 +11,16 @@ public class WorldMovementBlocks : WorldMovement
 	{
 		var newPosition = currentPosition + new Vector3 (unitVector.x * direction.x, 0, unitVector.y * direction.y);
 
-//		if (Physics.CheckBox(newPosition - new Vector3(0, unitVector.y, 0), unitVector * 0.5f)) {
-		if (Physics.CheckBox(newPosition, unitVector * boxChecksize)) {
-			return newPosition;
-		}
+		newPosition += new Vector3 (0, unitVector.y * (heightTolerance + 1), 0);
 
+		for (int i = 0; i <= heightTolerance * 2; i++) {
+			newPosition -= new Vector3 (0, unitVector.y, 0);
+
+			if (Physics.CheckBox(newPosition, unitVector * boxChecksize)) {
+				return newPosition;
+			}
+		}
+			
 		return currentPosition;
 	}
 	#endregion
