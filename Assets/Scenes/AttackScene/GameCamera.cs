3596375d258922b5e6aef6f264f2834da7610ddf
Ariel Coppes <ariel.coppes@gmail.com>
2017-04-23 11:22:15 -0300

    added tween to camera

--- Assets/Scenes/AttackScene/GameCamera.cs
+++ Assets/Scenes/AttackScene/GameCamera.cs
@@ -4,9 +4,37 @@ public class GameCamera : MonoBehaviour
 {
 	public Transform cameraTransform;
 
+	bool transitioning;
+
+	Vector3 targetPosition;
+
+	public float transitionSpeed = 10.0f;
+
+	public CameraInput cameraInput;
+
+	public AnimationCurve movementCurve;
+
 	public void CenterOn (Transform transform)
 	{
-		cameraTransform.position = transform.position;
+		targetPosition = transform.position;
+		transitioning = true;
+
+		if (cameraInput != null)
+			cameraInput.enabled = false;
+	}
+
+	void LateUpdate()
+	{
+		if (!transitioning)
+			return;
+		
+		cameraTransform.position = Vector3.Lerp (cameraTransform.position, targetPosition, movementCurve.Evaluate(Time.deltaTime * transitionSpeed));
+		transitioning = Vector3.Distance (cameraTransform.position, targetPosition) > 1.0f;
+
+		if (!transitioning) {
+			if (cameraInput != null)
+				cameraInput.enabled = true;
+		}
 	}
 	
 }
